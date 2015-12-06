44592cca48fc66f65d2fb504c0fc3e686ab53e19
Shamshel <codyray.herndon@gmail.com>
2015-12-06 22:33:06 +0000

    Implemented dynamic startbootstrap index page
    Implemented Startbootstrap-bare to pull the Post model from the Blogs app.
    The index page now displays the 5 latests posts in ascending order.
    As a correction from the previous commit, an Index model WAS implemented,
    but was removed to avoid any possibility of a circular dependency resulting
    from additional models in the Index page. The index now lives in
    Isaac_website and contains the tight coupling to this specific project,
    apps are still intended to be modular.

--- Isaac_website/views.py
+++ Isaac_website/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 from Blogs.models import Post
 
 def index(request):
-	return render(request, 'Isaac_website/index.html', { })
+	posts = Post.objects.filter(published=True).order_by('-published_date')[:5]
+	return render(request, 'Isaac_website/index.html', { 'posts':posts })
 
